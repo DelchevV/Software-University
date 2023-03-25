0685d070af24ef8690cb90d38ba23e77b49d6afb
Veselin Delchev <delchev.v5454@gmail.com>
2023-03-25 14:16:19 +0200

    Baked with tears and screams(not from joy :[)

--- JS Front End/HTTP and REST - Exercise/04.Blog/app.js
+++ JS Front End/HTTP and REST - Exercise/04.Blog/app.js
@@ -1,5 +1,133 @@
+// function attachEvents() {
+//     const loadBtn = document.getElementById('btnLoadPosts')
+//     const viewBtn=document.getElementById('btnViewPost')
+//     const posts = document.getElementById("posts")
+//     const postDetails= document.getElementById('post-title')
+//     const postCommets= document.getElementById('post-comments')
+//     const postbody= document.getElementById('post-body')
+//     const URL_POST= 'http://localhost:3030/jsonstore/blog/posts'
+//     const URL_COMMENTS= 'http://localhost:3030/jsonstore/blog/comments'
+
+//     loadBtn.addEventListener('click', loadHandler)
+//     viewBtn.addEventListener('click', viewHandler)
+
+
+
+//     function loadHandler(){
+//         posts.innerHTML=""
+//         fetch(URL_POST)
+//         .then((text)=>{
+//             return text.json()
+//         })
+//         .then((data)=>{
+//             for (line in data){
+//                 let option= document.createElement('option')
+//                 option.value=line
+//                 option.textContent=data[line].title
+//                 posts.appendChild(option)
+//             }
+//         })
+//     }
+
+//     function viewHandler(){
+//         let selectedOption = posts.options[posts.selectedIndex];
+        
+//          fetch(URL_COMMENTS)
+//          .then((info)=>{
+//             return info.json()
+//          })
+//          .then((data)=>{
+//             postCommets.innerHTML=''
+//             for (line in data){
+                
+//                 if (selectedOption.value===data[line].postId){
+//                     // postDetails.textContent=selectedOption.textContent
+//                     fetch(URL_POST)
+//                     .then((received)=>{
+//                         return received.json()
+//                     })
+//                     .then((jsonStr)=>{
+//                         for (l in jsonStr){
+//                             if (l === selectedOption.value){
+//                                 postDetails.textContent=jsonStr[l].title
+//                                 postbody.textContent=jsonStr[l].body
+//                             }
+//                         }
+//                     })
+//                     postbody.textContent=selectedOption
+//                     let li = document.createElement('li')
+//                     li.textContent=data[line].text
+//                     li.id=data[line].id
+//                     postCommets.appendChild(li)
+//                 }
+//             }
+//          })
+//     }
+// }
+
+// attachEvents();
+
+
 function attachEvents() {
-    // TODO:
+    const [btnLoadPosts, btnViewPost] = Array.from(document.querySelectorAll('button'))
+    const posts = document.querySelector('#posts')
+    const postTitle = document.querySelector('#post-title')
+    const postBody = document.querySelector('#post-body')
+    
+    let postInfo = {}
+    const postsApiUrl = 'http://localhost:3030/jsonstore/blog/posts'
+    const commentsApiUrl = 'http://localhost:3030/jsonstore/blog/comments'
+
+    function createElementWithValueAndTextContent(tag, value, textContent) {
+        const e = document.createElement(tag)
+        e.value = value
+        e.textContent = textContent
+        return e
+    }
+    
+    function findPost(selected) {
+        for (const key in postInfo) {
+            if (selected === postInfo[key].id) {
+                return postInfo[key]
+            }
+        }
+    }
+    
+    
+    btnLoadPosts.addEventListener('click', () => {
+        fetch(postsApiUrl).then(x => x.json()).then(x => {
+            postInfo = x
+            for (const key in x) {
+                posts.appendChild(createElementWithValueAndTextContent('option', x[key].id, x[key].title))
+            }
+        }).catch()
+    })
+
+    btnViewPost.addEventListener('click', () => {
+        fetch(commentsApiUrl).then(x => x.json()).then(x => {
+            let post = findPost(posts.value)
+            postTitle.textContent = post.title
+            postBody.textContent = post.body
+            const boby = document.querySelector('body')
+            const ul = document.querySelector('#post-comments')
+            
+            boby.removeChild(ul)
+            
+            const newUl = document.createElement('ul')
+            newUl.setAttribute('id', 'post-comments')
+            boby.appendChild(newUl)
+            const postComments = document.querySelector('#post-comments')
+            
+            for (const key in x) {
+                if (x[key].postId === posts.value) {
+                    const l = document.createElement('li')
+                    l.textContent = x[key].text
+                    postComments.appendChild(l)
+                }
+            }
+        }).catch()
+    })
+
 }
 
 attachEvents();
--- JS Front End/HTTP and REST - Exercise/07.Students/app.js
+++ JS Front End/HTTP and REST - Exercise/07.Students/app.js
@@ -1,5 +1,59 @@
 function attachEvents() {
-  // TODO:
+  const BASE_URL = 'http://localhost:3030/jsonstore/collections/students'
+  const submit = document.getElementsByTagName('button')[0]
+  const tbody = document.getElementsByTagName('tbody')[0]
+
+  loadEntities()
+
+  submit.addEventListener('click', submitHandler)
+
+  function loadEntities() {
+    tbody.innerHTML = ""
+    fetch(BASE_URL)
+      .then((info) => {
+        return info.json()
+      })
+      .then((data) => {
+        for (line in data) {
+          console.log(data[line])
+          let tr = document.createElement('tr')
+          let firstTd = document.createElement('td')
+          let seconTd = document.createElement('td')
+          let thirdTd = document.createElement('td')
+          let forthTd = document.createElement('td')
+
+          firstTd.textContent = data[line].firstName
+          seconTd.textContent = data[line].lastName
+          thirdTd.textContent = data[line].facultyNumber
+          forthTd.textContent = data[line].grade
+
+          tr.appendChild(firstTd)
+          tr.appendChild(seconTd)
+          tr.appendChild(thirdTd)
+          tr.appendChild(forthTd)
+
+          tbody.appendChild(tr)
+
+        }
+      })
+  }
+
+
+  function submitHandler() {
+    const firstName = document.querySelector('input[name="firstName"]').value
+    const lastName = document.querySelector('input[name="lastName"]').value
+    const facultyNumber = document.querySelector('input[name="facultyNumber"]').value
+    const grade = document.querySelector('input[name="grade"]').value
+
+    let httpHeader = {
+      method: "POST",
+      body: JSON.stringify({ firstName, lastName, facultyNumber, grade })
+    }
+    fetch(BASE_URL, httpHeader)
+      .then(() => {
+        loadEntities()
+      })
+  }
 }
 
 attachEvents();
